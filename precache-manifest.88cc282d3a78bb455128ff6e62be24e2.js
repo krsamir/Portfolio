8477489d34b62d718b6a834728e12562aa4092df
krsamir <samir1741012321@gmail.com>
2020-06-25 22:16:37 +0530

    Updates

---
+++ precache-manifest.88cc282d3a78bb455128ff6e62be24e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61e8f5c9d729d1e1c96f37c64130074",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c9d5a795d098f67ce373",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "982f8db5562c50b0d968",
+    "url": "/my-app/static/js/2.c7663357.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.c7663357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d5a795d098f67ce373",
+    "url": "/my-app/static/js/main.423e2330.chunk.js"
+  },
+  {
+    "revision": "01bbda6f2f23ad0cf967",
+    "url": "/my-app/static/js/runtime-main.43a97011.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
